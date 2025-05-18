77ad1c4e6db5343f86217bea24755479fdde7671
Fernando Carazo <fernando.carazom@gmail.com>
2025-05-18 21:15:44 +0200

    .github/workflows/daily_run.yml

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+
+import pandas as pd
 from crewai import LLM
 from dotenv import load_dotenv
 
@@ -5,7 +8,7 @@ from iglesia.agents import create_iglesia_content_crew
 from iglesia.utils import obtener_todos_los_textos
 
 
-def main(debug=True):
+def main(debug=False):
     load_dotenv()
     LLM_used = LLM(
         model="gpt-4.1-nano",
@@ -17,19 +20,33 @@ def main(debug=True):
         "Angelus": "https://www.vatican.va/content/leo-xiv/es/angelus/2025.html",
         "Discurso": "https://www.vatican.va/content/leo-xiv/es/speeches/2025/may.index.html",
     }
+    fecha_de_hoy = pd.Timestamp.now().strftime("%Y-%m-%d")
 
+    # crear carpeta dentro de summaries con la fecha de hoy
+    os.makedirs(f"summaries/{fecha_de_hoy}", exist_ok=True)
+    
     df = obtener_todos_los_textos(urls)
     df = df[df["titulo"].str.len() > 10]
     df = df.reset_index(drop=True)
     print(df)
     if debug:
         df = df[:2]
-
     df["filename"] = df.apply(
         lambda x: f"{x['fecha']}_{x['tipo'].replace(' ', '_').lower()}_{x['titulo'].replace(' ', '_').lower()}",
         axis=1,
     )
-    df.to_csv("summaries/iglesia.csv", index=False)
+    # filtrar los df de la última semana
+    print(df)
+
+    run_date = pd.Timestamp('today').normalize()
+    df['fecha_dt'] = pd.to_datetime(df['fecha'])
+    df = df[(df['fecha_dt'] >= (run_date - pd.Timedelta(days=7))) & (df['fecha_dt'] < run_date)]
+    
+    # por ejemplo, si hoy es lunes 18 de mayo, se filtran los df de lunes 11 de mayo
+    # qué dias se incluesn: lunes 11, martes 12, miércoles 13, jueves 14, viernes 15, sábado 16 y domingo 17
+    df = df.drop(columns=["fecha_dt"])
+    print(df)
+    df.to_csv(f"summaries/{fecha_de_hoy}/iglesia.csv", index=False)
     iglesia_content_crew = create_iglesia_content_crew(df, LLM_used)
     _ = iglesia_content_crew.kickoff()
 
