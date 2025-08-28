39c965a5f7b48453134a7d171bef3e7c7b1e43f8
Fernando Carazo <fernando.carazo>
2025-08-28 22:32:39 +0200

    update bugs

--- iglesia/cognito_utils.py
+++ iglesia/cognito_utils.py
@@ -1,5 +1,6 @@
-import boto3
 import os
+
+import boto3
 import pandas as pd
 from dotenv import load_dotenv
 
@@ -8,20 +9,60 @@ load_dotenv()
 # Lista ampliada de nombres compuestos comunes
 COMMON_COMPOUND_NAMES = {
     # Masculinos
-    "juan pablo", "juan carlos", "juan josé", "juan manuel", "juan antonio",
-    "josé luis", "josé manuel", "josé antonio", "josé carlos", "josé maría",
-    "luis miguel", "luis fernando", "miguel ángel", "carlos alberto", "pedro josé",
-    "ángel david", "carlos andrés", "juan andrés", "jose ángel", "jose fernando",
-    "juan gabriel", "josé andrés", "francisco javier", "juan francisco",
-
+    "juan pablo",
+    "juan carlos",
+    "juan josé",
+    "juan manuel",
+    "juan antonio",
+    "josé luis",
+    "josé manuel",
+    "josé antonio",
+    "josé carlos",
+    "josé maría",
+    "luis miguel",
+    "luis fernando",
+    "miguel ángel",
+    "carlos alberto",
+    "pedro josé",
+    "ángel david",
+    "carlos andrés",
+    "juan andrés",
+    "jose ángel",
+    "jose fernando",
+    "juan gabriel",
+    "josé andrés",
+    "francisco javier",
+    "juan francisco",
     # Femeninos
-    "maría josé", "maría jesús", "maría fernanda", "maría elena", "maría teresa",
-    "maría isabel", "maría eugenia", "maría luisa", "maría dolores", "maría del mar",
-    "ana belén", "ana maría", "ana isabel", "ana sofía", "ana laura", "ana paula",
-    "rosa maría", "laura sofía", "maría de los ángeles", "maría del carmen",
-    "maría josefa", "maría auxiliadora", "maría concepción", "maría mercedes",
-    "maría cristina", "maría inés", "maría gloria"
+    "maría josé",
+    "maría jesús",
+    "maría fernanda",
+    "maría elena",
+    "maría teresa",
+    "maría isabel",
+    "maría eugenia",
+    "maría luisa",
+    "maría dolores",
+    "maría del mar",
+    "ana belén",
+    "ana maría",
+    "ana isabel",
+    "ana sofía",
+    "ana laura",
+    "ana paula",
+    "rosa maría",
+    "laura sofía",
+    "maría de los ángeles",
+    "maría del carmen",
+    "maría josefa",
+    "maría auxiliadora",
+    "maría concepción",
+    "maría mercedes",
+    "maría cristina",
+    "maría inés",
+    "maría gloria",
 }
+
 
 def extract_first_name(full_name):
     """
@@ -38,6 +79,7 @@ def extract_first_name(full_name):
             return candidate.title()
 
     return parts[0].capitalize()
+
 
 def cognito_get_verified_emails(only_verified: bool = True):
     """
@@ -64,7 +106,7 @@ def cognito_get_verified_emails(only_verified: bool = True):
     for page in page_iterator:
         for user in page["Users"]:
             attrs = {attr["Name"]: attr["Value"] for attr in user["Attributes"]}
-            
+
             if only_verified and attrs.get("email_verified") == "true":
                 email = attrs.get("email")
                 full_name = attrs.get("name", "")
@@ -73,7 +115,8 @@ def cognito_get_verified_emails(only_verified: bool = True):
                 if email:
                     names.append(first_name)
                     emails.append(email)
-            elif not only_verified:
+            # elif not only_verified:
+            else:
                 email = attrs.get("email")
                 full_name = attrs.get("name", "")
                 first_name = extract_first_name(full_name)
@@ -81,8 +124,6 @@ def cognito_get_verified_emails(only_verified: bool = True):
                 if email:
                     names.append(first_name)
                     emails.append(email)
-            else:
-                print(f"Skipping user {user['Username']} due to unverified email.")
 
     df = pd.DataFrame({"nombre": names, "email": emails})
     return df
